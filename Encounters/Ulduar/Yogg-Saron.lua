ed49072d4b03941347d7dbf5d0a57aad899c088d
kollektiv <kollektiv@pc>
2009-04-18 01:41:10 +0000

    Updating yogg-saron

--- Encounters/Ulduar/Yogg-Saron.lua
+++ Encounters/Ulduar/Yogg-Saron.lua
@@ -5,7 +5,7 @@ do
 		zone = "Ulduar", 
 		name = "Yogg-Saron", 
 		title = "Yogg-Saron", 
-		tracing = {"Yogg-Saron",},
+		tracing = {"Sara",},
 		triggers = {
 			scan = "Sara",
 			yell = "^The time to strike at the head of the beast",
@@ -13,8 +13,14 @@ do
 		onactivate = {
 			leavecombat = true,
 		},
-		userdata = {},
-		onstart = {},
+		userdata = {
+			portaltime = {78,90,loop = false},
+		},
+		onstart = {
+			[1] = {
+				{alert = "enragecd"},
+			},
+		},
 		alerts = {
 			lunaticgazewarn = {
 				var = "lunaticgazewarn",
@@ -22,26 +28,91 @@ do
 				type = "centerpopup",
 				text = "Gaze: LOOK AWAY!",
 				time = 4,
-				color1 = "ORANGE",
+				color1 = "PURPLE",
 				sound = "ALERT1",
 			},
 			brainlinkdur = {
 				var = "brainlinkdur",
 				varname = "Brain Link on self",
-				type = "dropdown",
+				type = "centerpopup",
 				text = "Brain Link: YOU",
 				time = 30,
-				flashtime = 0,
 				color1 = "BLUE",
+				color2 = "INDIGO",
 				sound = "ALERT3",
 			},
 			yoggsaronarrives = {
 				var = "yoggsaronarrives",
 				varname = "Yogg'Saron arrival",
-				type = "dropdown",
+				type = "centerpopup",
 				text = "Yogg'Saron Arrives",
 				time = 20,
+				color1 = "GREY",
+			},
+			enragecd = {
+				var = "enragecd",
+				varname = "Enrage",
+				type = "dropdown",
+				text = "Enrage",
+				time = 900,
+				flashtime = 10,
+				color1 = "RED",
+			},
+			portalcd = {
+				var = "portalcd",
+				varname = "Portal cooldown",
+				type = "dropdown",
+				text = "Next Portal",
+				time = "<portaltime>",
+				flashtime = 10,
+				sound = "ALERT2",
+				color1 = "MAGENTA",
+				color2 = "DCYAN",
+			},
+			weakeneddur = {
+				var = "weakeneddur",
+				varname = "Weakened duration",
+				type = "centerpopup",
+				text = "Weakened!",
+				time = 45,
+				flashtime = 10,
+				color1 = "ORANGE",
+				color2 = "YELLOW",
+			},
+			inducewarn = {
+				var = "inducewarn",
+				varname = "Induce Madness cast",
+				type = "dropdown",
+				text = "Induce Madness cast",
+				time = 60,
 				flashtime = 5,
+				color1 = "BROWN",
+				color2 = "MIDGREY",
+			},
+			squeezewarn = {
+				var = "squeezewarn",
+				varname = "Squeeze warning",
+				type = "simple",
+				text = "Squeeze: #5#",
+				time = 1.5,
+				color1 = "AQUA",
+			},
+			maladywarnself = {
+				var = "maladywarn",
+				varname = "Malady of the Mind warn",
+				type = "simple",
+				text = "Malady of the Mind: YOU!",
+				time = 1.5,
+				sound = "ALERT5",
+				color1 = "GREEN",
+			},
+			maladywarnother = {
+				var = "maladywarn",
+				varname = "Malady of the Mind warn",
+				type = "simple",
+				text = "Malady of the Mind: #5#",
+				time = 1.5,
+				sound = "ALERT5",
 				color1 = "GREEN",
 			},
 		},
@@ -69,14 +140,65 @@ do
 					},
 				},
 			},
-			-- Phase 2 beginning
 			[3] = {
 				type = "event",
 				event = "CHAT_MSG_MONSTER_YELL",
 				execute = {
+					-- Phase 2
 					[1] = {
+						{tracing = {"Yogg-Saron","Sara"}},
 						{expect = {"#1#","find","^I am the lucid dream"}},
 						{alert = "yoggsaronarrives"},
+						{alert = "portalcd"},
+					},
+					-- Phase 3
+					[2] = {
+						{expect = {"#1#","find","^Look upon the true face"}},
+						{quash = "inducewarn"},
+						{quash = "portalcd"},
+					},
+				},
+			},
+			[4] = {
+				type = "event",
+				event = "EMOTE",
+				execute = {
+					-- Portal
+					[1] = {
+						{expect = {"#1#","find","^Portals open into Yogg-Saron"}},
+						{alert = "inducewarn"},
+						{alert = "portalcd"},
+					},
+					[2] = {
+						{expect = {"#1#","find","^The Illusion shatters and a path"}},
+						{quash = "inducewarn"},
+						{alert = "weakeneddur"},
+					},
+				},
+			},
+			[5] = {
+				type = "combatevent",
+				eventtype = "SPELL_AURA_APPLIED",
+				spellid = 64126,
+				execute = {
+					[1] = {
+						{expect = {"#4#","~=","&playerguid&"}},
+						{alert = "squeezewarn"},
+					},
+				},
+			},
+			[6] = {
+				type = "combatevent",
+				eventtype = "SPELL_AURA_APPLIED",
+				spellid = 63830,
+				execute = {
+					[1] = {
+						{expect = {"#4#","==","&playerguid&"}},
+						{alert = "maladywarnself"},
+					},
+					[2] = {
+						{expect = {"#4#","~=","&playerguid&"}},
+						{alert = "maladywarnother"},
 					},
 				},
 			},
