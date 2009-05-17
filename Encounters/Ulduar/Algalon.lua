e5545db01ef2b77ad21057fa3faf1dcf7a88379a
kollektiv <kollektiv@pc>
2009-05-17 03:39:18 +0000

    Updating Algalon the Observer

--- Encounters/Ulduar/Algalon.lua
+++ Encounters/Ulduar/Algalon.lua
@@ -7,22 +7,60 @@ do
 		title = "Algalon the Observer", 
 		tracing = {"Algalon the Observer",},
 		triggers = {
-			scan = "Algalon the Observer", 
+			scan = "Algalon the Observer",
+			yell = "^Your actions are illogical. All possible results",
 		},
 		onactivate = {
-			autostart = true,
-			autostop = true,
 			leavecombat = true,
 		},
 		userdata = {},
 		onstart = {},
-		alerts = {},
+		alerts = {
+			bigbangwarn = {
+				var = "bigbangwarn",
+				varname = "Big Bang cast",
+				type = "centerpopup",
+				text = "Big Bang Cast",
+				time = 8,
+				flashtime = 8,
+				sound = "ALERT5",
+				color1 = "ORANGE",
+				color2 = "BROWN",
+			},
+			--[[
+			cosmicsmashwarn = {
+			},
+			]]
+		},
+		events = {
+			[1] = {
+				type = "event",
+				event = "EMOTE",
+				execute = {
+					-- Collapsing Stars
+					[1] = {
+						{expect = {"#1#","find","begins to Summon Collapsing Stars!"}},
+					},
+					-- Cosmic Smash
+					[2] = {
+						{expect = {"#1#","find","begins to cast Cosmic Smash!"}},
+					},
+					-- Big Bang
+					[3] = {
+						{expect = {"#1#","find","begins to cast Big Bang!"}},
+						{alert = "bigbangwarn"},
+					},
+				},
+			},
+		}
 	}
 	-- Big Bang
-		-- 8 second cast that does 100k damage
+		-- 8 second cast that wdoes 100k damage
+		-- Have to be in black hole to avoid it
 	-- Phase Punch
 	-- Quantum Strike
 	-- Black Hole Explosion
 	-- Cosmic Smash
+		-- Red void zones on the ground
 	DXE:RegisterEncounter(data)
 end
