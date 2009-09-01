e39610489eb54e416c919a6dd074cb034307f07b
kollektiv <kollektiv@pc>
2009-09-01 17:54:52 -0400

    Anub: Completed first version

--- Encounters/Coliseum/Anub.lua
+++ Encounters/Coliseum/Anub.lua
@@ -1,4 +1,3 @@
---[=[
 do
 	local L,SN,ST = DXE.L,DXE.SN,DXE.ST
 	local data = {
@@ -9,22 +8,68 @@ do
 		name = L["Anub'arak"], 
 		triggers = {
 			scan = {
+				34660, -- Anub
 			}, 
 		},
 		onactivate = {
-			tracing = {--[[todo]]},
+			tracing = {34660},
+			tracerstart = true,
 			combatstop = true,
 		},
-		userdata = {},
-		onstart = {
-		},
 		alerts = {
-
+			pursueself = {
+				varname = format(L["%s on self"],SN[62374]),
+				type = "centerpopup",
+				time = 60,
+				flashtime = 60
+				text = format("%s: #5#! %s!",SN[62374],L["RUN"]),
+				color1 = "BROWN",
+				color2 = "GREY",
+				icon = ST[67574],
+			}
+			pursueother = {
+				varname = format(L["%s on others"],SN[62374]),
+				type = "dropdown",
+				text = format("%s: #5#!",SN[62374]),
+				time = 60,
+				flashtime = 15,
+				color1 = "BROWN",
+				icon = ST[67574],
+			},
 		},
-		events = { 
+		events = {
+			{
+				type = "combatevent",
+				eventtype = "SPELL_AURA_APPLIED",
+				spellid = 67574,
+				execute = {
+					{
+						"expect",{"#4#","==","&playerguid&"},
+						"alert","pursueself",
+					},
+					{
+						"expect",{"#4#","~=","&playerguid&"},
+						"alert","pursueothers",
+					},
+				},
+			},
+			{
+				type = "combatevent",
+				eventtype = "SPELL_AURA_REMOVED",
+				spellid = 67574,
+				execute = {
+					{
+						"expect",{"#4#","==","&playerguid&"},
+						"quash","pursueself",
+					},
+					{
+						"expect",{"#4#","~=","&playerguid&"},
+						"quash","pursueothers",
+					},
+				},
+			},
 		},
 	}
 
 	DXE:RegisterEncounter(data)
 end
-]=]
