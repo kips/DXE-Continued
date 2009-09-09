4bb5b695c2cf78c811f0f9c647261cf53541391b
kollektiv <kollektiv@pc>
2009-09-09 12:05:17 -0400

    Northrend Beasts: Adjusted phase 1 hard mode timer again

--- Encounters/Coliseum/Northrend_Beasts.lua
+++ Encounters/Coliseum/Northrend_Beasts.lua
@@ -1,7 +1,7 @@
 do
 	local L,SN,ST = DXE.L,DXE.SN,DXE.ST
 	local data = {
-		version = 326,
+		version = 327,
 		key = "northrendbeasts", 
 		zone = L["Trial of the Crusader"], 
 		category = L["Coliseum"],
@@ -40,7 +40,7 @@ do
 		onstart = {
 			{
 				"expect",{"&difficulty&",">=","3"},
-				"set",{enragetime = 140},
+				"set",{enragetime = 143},
 			},
 			{
 				"expect",{"&difficulty&","<=","2"},
