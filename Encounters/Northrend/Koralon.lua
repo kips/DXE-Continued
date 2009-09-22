4dbe1c37a7c00adad7e647de8dd70cce31de1257
kollektiv <kollektiv@pc>
2009-09-22 17:32:14 -0400

    Koralon: Added Flaming Cinder on self

--- Encounters/Northrend/Koralon.lua
+++ Encounters/Northrend/Koralon.lua
@@ -2,7 +2,7 @@
 do
 	local L,SN,ST = DXE.L,DXE.SN,DXE.ST
 	local data = {
-		version = 1,
+		version = 2,
 		key = "koralon", 
 		zone = L["Vault of Archavon"], 
 		category = L["Northrend"],
@@ -16,6 +16,32 @@ do
 			tracing = {35013},
 			tracerstart = true,
 			combatstop = true,
+		},
+		alerts = {
+			flamingcinderself = {
+				varname = format(L["%s on self"],SN[67332]),
+				text = format("%s: %s!",SN[67332],L["YOU"]),
+				type = "simple",
+				time = 3,
+				throttle = 3,
+				color1 = "ORANGE",
+				sound = "ALERT1",
+				flashscreen = true,
+				icon = ST[67332],
+			},
+		},
+		events = {
+			{
+				type = "combatevent",
+				eventtype = "SPELL_AURA_APPLIED",
+				spellid = {67332,66684},
+				execute = {
+					{
+						"expect",{"#4#","==","&playerguid&"},
+						"alert","flamingcinderself",
+					},
+				},
+			},
 		},
 	}
 
