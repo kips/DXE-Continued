15e43864c787189631bf0a9e5eb40f2577636f93
kollektiv <kollektiv@pc>
2009-12-07 16:14:58 -0500

    Revert back to the 3.2 method of extracting npc ids from guids

--- Encounters/Citadel/Saurfang.lua
+++ Encounters/Citadel/Saurfang.lua
@@ -2,10 +2,10 @@ do
 	local L,SN,ST = DXE.L,DXE.SN,DXE.ST
 	local data = {
 		version = 1,
-		key = "deathbringer", 
+		key = "saurfang", 
 		zone = L["Icecrown Citadel"], 
 		category = L["Citadel"], 
-		name = L["Deathbringer"], 
+		name = L["Saurfang"], 
 		triggers = {
 			--scan = ,
 			--yell = ,
