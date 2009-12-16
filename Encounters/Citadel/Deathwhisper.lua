9485e05bf5611bb97496464adc81afa0be47c976
kollektiv <kollektiv@pc>
2009-12-16 16:50:00 -0500

    Deathwhisper: Added Cult spawn timers and mana barrier removal warning

--- Encounters/Citadel/Deathwhisper.lua
+++ Encounters/Citadel/Deathwhisper.lua
@@ -1,7 +1,7 @@
 do
 	local L,SN,ST = DXE.L,DXE.SN,DXE.ST
 	local data = {
-		version = 2,
+		version = 3,
 		key = "deathwhisper", 
 		zone = L["Icecrown Citadel"], 
 		category = L["Citadel"], 
@@ -11,6 +11,23 @@ do
 				36855, -- Lady Deathwhisper
 			},
 			yell = L["^What is this disturbance"],
+		},
+		userdata = {
+			culttime = {7,60,loop = false},
+		},
+		onstart = {
+			{
+				"alert","cultcd",
+				"scheduletimer",{"firecult",7},
+			},
+		},
+		timers = {
+			firecult = {
+				{
+					"alert","cultcd",
+					"scheduletimer",{"firecult",60},
+				},
+			},
 		},
 		onactivate = {
 			combatstop = true,
@@ -27,6 +44,25 @@ do
 				icon = ST[71001],
 				flashscreen = true,
 			},
+			cultcd = {
+				varname = format(L["%s Spawns"],L["Cult"]),
+				text = format(L["%s Spawns"],L["Cult"]),
+				type = "dropdown",
+				time = "<culttime>",
+				flashtime = 10,
+				sound = "ALERT2",
+				color1 = "BROWN",
+				icon = ST[61131],
+			},
+			manabarrierwarn = {
+				varname = format(L["%s Removal"],SN[70842]),
+				text = format(L["%s Removed"],SN[70842]).."!",
+				type = "simple",
+				time = 3,
+				sound = "ALERT3",
+				color1 = "TEAL",
+				icon = ST[70842],
+			},
 		},
 		events = {
 			-- Death and Decay self
@@ -41,6 +77,19 @@ do
 					{
 						"expect",{"#4#","==","&playerguid&"},
 						"alert","dndself",
+					},
+				},
+			},
+			-- Mana Barrier
+			{
+				type = "combatevent",
+				eventtype = "SPELL_AURA_REMOVED",
+				spellid = 70842,
+				execute = {
+					{
+						"alert","manabarrierwarn",
+						"quash","cultcd",
+						"canceltimer","firecult",
 					},
 				},
 			},
