3758279539de21762288400976cf3e97f0ae3ee8
kollektiv <kollektiv@pc>
2009-12-21 20:08:17 -0500

    Gunship Battle: Added reaver/marine and sergeant spawn timers

--- Encounters/Citadel/Gunship_Battle.lua
+++ Encounters/Citadel/Gunship_Battle.lua
@@ -3,15 +3,21 @@ do
 
 	local faction = UnitFactionGroup("player")
 
-	local defeat_msg
+	local defeat_msg,portal_msg,add,portal_icon
 	if faction == "Alliance" then
 		defeat_msg = L["^Don't say I didn't warn ya"]
+		portalmsg = L["^Reavers, Sergeants, attack"]
+		add = L["Reaver"]
+		portal_icon = "Interface\\Icons\\achievement_pvp_h_04"
 	elseif faction == "Horde" then
 		defeat_msg = L["^The Alliance falter"]
+		portal_msg = L["^Marines, Sergeants, attack"]
+		add = L["Marine"] 
+		portal_icon = "Interface\\Icons\\achievement_pvp_a_04"
 	end
 
 	local data = {
-		version = 2,
+		version = 3,
 		key = "gunshipbattle", 
 		zone = L["Icecrown Citadel"], 
 		category = L["Citadel"], 
@@ -32,6 +38,14 @@ do
 			unittracing = {"boss1","boss2"},
 			defeat = defeat_msg,
 		},
+		userdata = {
+			portaltime = {12,60,loop = false}, -- TODO: initial time
+		},
+		onstart = {
+			{
+				"alert","portalcd",
+			},
+		},
 		alerts = {
 			belowzerowarn = {
 				varname = format(L["%s Channel"],SN[69705]),
@@ -42,6 +56,16 @@ do
 				color1 = "BLUE",
 				sound = "ALERT5",
 				icon = ST[69705],
+			},
+			portalcd = {
+				varname = format(L["%s Spawns"],add.."/"..L["Sergeant"]),
+				type = "dropdown",
+				text = format(L["%s Spawns"],add.."/"..L["Sergeant"]),
+				time = "<portaltime>",
+				flashtime = 10,
+				color1 = "GOLD",
+				sound = "ALERT1",
+				icon = portal_icon,
 			},
 		},
 		events = {
@@ -64,6 +88,17 @@ do
 				execute = {
 					{
 						"quash","belowzerowarn",
+					},
+				},
+			},
+			-- Portals
+			{
+				type = "event",
+				event = "YELL",
+				execute = {
+					{
+						"expect",{"#1#","==",portal_msg},
+						"alert","portalcd",
 					},
 				},
 			},
