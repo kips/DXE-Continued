0241325332a025145d484eee26e36004a5f54305
kollektiv <kollektiv@pc>
2010-01-06 23:59:31 -0500

    Precious: Added encounter. It is pretty much the same as Stinky

--- Encounters/Citadel/Trash.lua
+++ Encounters/Citadel/Trash.lua
@@ -49,96 +49,169 @@ do
 end
 
 do
-	local data = {
-		version = 2,
-		key = "icctrashtwo",
-		zone = L["Icecrown Citadel"],
-		category = L["Citadel"],
-		name = format(L["%s (T)"],L["Stinky"]),
-		triggers = {
-			scan = {
-				37025, -- Stinky
-			},
-		},
-		onactivate = {
-			tracing = {37025}, -- Stinky
-			tracerstart = true,
-			combatstop = true,
-		},
-		userdata = {
-			mortaltext = "",
-		},
-		alerts = {
-			decimatewarn = {
-				varname = format(L["%s Cast"],SN[71123]),
-				type = "centerpopup",
-				text = format(L["%s Cast"],SN[71123]),
-				time = 3,
-				flashtime = 3,
-				color1 = "PURPLE",
-				sound = "ALERT5",
-				flashscreen = true,
-				icon = ST[71123],
-			},
-			mortalwarn = {
-				varname = format(L["%s Warning"],SN[71127]),
-				type = "simple",
-				text = "<mortaltext>",
-				time = 3,
-				color1 = "RED",
-				icon = ST[71127],
-			},
-		},
-		events = {
-			-- Decimate
+	local decimate_event = {
+		type = "combatevent",
+		eventtype = "SPELL_CAST_START",
+		spellid = 71123, -- 10/25
+		execute = {
 			{
-				type = "combatevent",
-				eventtype = "SPELL_CAST_START",
-				spellid = 71123, -- 10/25
-				execute = {
-					{
-						"alert","decimatewarn"
-					},
-				}
+				"alert","decimatewarn"
 			},
-			-- Mortal Wound
+		}
+	}
+	
+	local mortal_wound_event = {
+		type = "combatevent",
+		eventtype = "SPELL_AURA_APPLIED",
+		spellid = 71127,
+		execute = {
 			{
-				type = "combatevent",
-				eventtype = "SPELL_AURA_APPLIED",
-				spellid = 71127,
-				execute = {
-					{
-						"expect",{"#4#","==","&playerguid&"},
-						"set",{mortaltext = format("%s: %s!",SN[71127],L["YOU"])},
-						"alert","mortalwarn",
-					},
-					{
-						"expect",{"#4#","~=","&playerguid&"},
-						"set",{mortaltext = format("%s: #5#!",SN[71127])},
-						"alert","mortalwarn",
-					},
-				},
+				"expect",{"#4#","==","&playerguid&"},
+				"set",{mortaltext = format("%s: %s!",SN[71127],L["YOU"])},
+				"alert","mortalwarn",
 			},
-			-- Mortal Wounds applications
 			{
-				type = "combatevent",
-				eventtype = "SPELL_AURA_APPLIED_DOSE",
-				spellid = 71127,
-				execute = {
-					{
-						"expect",{"#4#","==","&playerguid&"},
-						"set",{mortaltext = format("%s: %s! %s!",SN[71127],L["YOU"],format(L["%s Stacks"],"#11#"))},
-						"alert","mortalwarn",
-					},
-					{
-						"expect",{"#4#","~=","&playerguid&"},
-						"set",{mortaltext = format("%s: #5#! %s!",SN[71127],format(L["%s Stacks"],"#11#")) },
-						"alert","mortalwarn",
-					},
-				},
+				"expect",{"#4#","~=","&playerguid&"},
+				"set",{mortaltext = format("%s: #5#!",SN[71127])},
+				"alert","mortalwarn",
 			},
 		},
 	}
 
-	DXE:RegisterEncounter(data)
+	local mortal_wound_dose_event = {
+		type = "combatevent",
+		eventtype = "SPELL_AURA_APPLIED_DOSE",
+		spellid = 71127,
+		execute = {
+			{
+				"expect",{"#4#","==","&playerguid&"},
+				"set",{mortaltext = format("%s: %s! %s!",SN[71127],L["YOU"],format(L["%s Stacks"],"#11#"))},
+				"alert","mortalwarn",
+			},
+			{
+				"expect",{"#4#","~=","&playerguid&"},
+				"set",{mortaltext = format("%s: #5#! %s!",SN[71127],format(L["%s Stacks"],"#11#")) },
+				"alert","mortalwarn",
+			},
+		},
+	}
+
+	local decimatewarn = {
+		varname = format(L["%s Cast"],SN[71123]),
+		type = "centerpopup",
+		text = format(L["%s Cast"],SN[71123]),
+		time = 3,
+		flashtime = 3,
+		color1 = "PURPLE",
+		sound = "ALERT5",
+		flashscreen = true,
+		icon = ST[71123],
+	}
+	local mortalwarn = {
+		varname = format(L["%s Warning"],SN[71127]),
+		type = "simple",
+		text = "<mortaltext>",
+		time = 3,
+		color1 = "RED",
+		icon = ST[71127],
+	}
+
+	do
+		local data = {
+			version = 3,
+			key = "icctrashtwo",
+			zone = L["Icecrown Citadel"],
+			category = L["Citadel"],
+			name = format(L["%s (T)"],L["Stinky"]),
+			triggers = {
+				scan = {
+					37025, -- Stinky
+				},
+			},
+			onactivate = {
+				tracing = {
+					37025, -- Stinky
+				}, 
+				tracerstart = true,
+				combatstop = true,
+			},
+			userdata = {
+				mortaltext = "",
+			},
+			alerts = {
+				decimatewarn = decimatewarn,
+				mortalwarn = mortalwarn,
+			},
+			events = {
+				-- Decimate
+				decimate_event,
+				-- Mortal Wound
+				mortal_wound_event,
+				-- Mortal Wounds applications
+				mortal_wound_dose_event,
+			},
+		}
+
+		DXE:RegisterEncounter(data)
+	end
+
+	do
+		local data = {
+			version = 1,
+			key = "icctrashthree",
+			zone = L["Icecrown Citadel"],
+			category = L["Citadel"],
+			name = format(L["%s (T)"],L["Precious"]),
+			triggers = {
+				scan = {
+					37217, -- Precious
+				},
+			},
+			onactivate = {
+				tracing = {
+					37217, -- Precious
+				}, 
+				tracerstart = true,
+				combatstop = true,
+			},
+			userdata = {
+				mortaltext = "",
+				awakentime = {28,20,loop = false},
+			},
+			alerts = {
+				decimatewarn = decimatewarn,
+				mortalwarn = mortalwarn,
+				awakencd = {
+					varname = format(L["%s Cooldown"],SN[71159]),
+					type = "dropdown",
+					text = format(L["%s Cooldown"],SN[71159]),
+					time = "<awakentime>",
+					flashtime = 10,
+					color1 = "GREY",
+					icon = ST[71159],
+				}
+			},
+			events = {
+				-- Decimate
+				decimate_event,
+				-- Mortal Wound
+				mortal_wound_event,
+				-- Mortal Wounds applications
+				mortal_wound_dose_event,
+				-- Awaken Plagued Zombie
+				{
+					type = "event",
+					event = "EMOTE",
+					execute = {
+						{
+							"expect",{"#2#","==",L["Precious"]},
+							"alert","awakencd",
+						},
+					},
+				},
+			},
+		}
+
+		DXE:RegisterEncounter(data)
+	end
 end
