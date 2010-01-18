8b5451657bb10e91ca5b376052497c90b06cc0e4
kollektiv <kollektiv@pc>
2010-01-18 17:49:26 -0500

    added error checking to localization script

--- Scripts/localize.rb
+++ Scripts/localize.rb
@@ -9,16 +9,30 @@ require File.join(File.dirname(__FILE__),'..','..','api_key') # for API_KEY
 raise "Missing API_KEY" unless API_KEY
 puts "API_KEY: #{API_KEY}"
 
+def validate_response(response)
+	body = response.class == String ? response : response.body
+	raise "API_KEY is invalid" if body =~ /www\.wowace\.com\/home\/login/
+	true
+end
+
 slug = "deus-vox-encounters"
-locale_uri = "http://www.wowace.com/addons/#{slug}/localization/import/"
-locale_uri_api_key = "#{locale_uri}/?api-key=#{API_KEY}"
+locale_uri = URI.parse "http://www.wowace.com/addons/#{slug}/localization/import/"
+locale_uri_api_key = URI.parse "#{locale_uri.to_s}/?api-key=#{API_KEY}"
 locale_regex = /L(?:\.(\w+?))?\["(.+?)"\]/
 
 # map namespace names to namespace values
 namespace_values = Hash.new
 select_regex = /<select.*?namespace.*>.*?<\/select>/
 option_regex = /<option.*?value="(\d+)">(\w+)<\/option>/
-Net::HTTP.get(URI.parse(locale_uri_api_key)).match(select_regex)[0].scan(option_regex) { |value, name| namespace_values[name] = value }
+
+begin
+	response = Net::HTTP.get(locale_uri_api_key)
+	validate_response(response)
+	response.match(select_regex)[0].scan(option_regex) { |value, name| namespace_values[name] = value }
+rescue StandardError => e
+	puts "error: unable to grab namespace values - #{e.message}"
+	exit
+end
 
 blacklist = {
 	"Template.lua"          => true,
@@ -27,7 +41,6 @@ blacklist = {
 	"Tests.lua"             => true,
 }
 
-spacing = "   "
 locales = Hash.new
 # find all phrases and namespace contexts
 puts "Scanning for localization phrases"
@@ -39,21 +52,26 @@ Dir[File.join(File.dirname(__FILE__),'..','**','*.lua')].each do |filename|
 			locales[namespace || 'main'] << phrase
 		end	
 	end
-	puts "#{spacing}#{filename.match(/scripts#{File::SEPARATOR}\.\.#{File::SEPARATOR}(.+)/i)[1].ljust(60)} #{"DONE".rjust(10)}"
+	puts "   #{filename.match(/#{File.join('scripts','..','(.+)')}/i)[1].ljust(60)} #{"DONE".rjust(10)}"
 end
 
 locales.each_pair do |namespace, phrases|
 	if namespace_values[namespace]
-		content = phrases.uniq.collect { |p| "L[\"#{p}\"] = true" }.join("\n")
-		puts "sending phrases for namespace '#{namespace}'"
-		Net::HTTP.post_form(URI.parse(locale_uri),{
-			"api-key"           => API_KEY,
-			"delete_unimported" => "y",
-			"format"            => "lua_additive_table",
-			"language" 			  => "1",
-			"text"              => content,
-			"namespace"         => namespace_values[namespace],
-		})
+		uniq_phrases = phrases.uniq
+		content = uniq_phrases.collect { |p| "L[\"#{p}\"] = true" }.join("\n")
+		begin
+			response = Net::HTTP.post_form(locale_uri,{
+				"api-key"           => API_KEY,
+				"delete_unimported" => "y",
+				"format"            => "lua_additive_table",
+				"language" 			  => "1",
+				"text"              => content,
+				"namespace"         => namespace_values[namespace],
+			})
+			puts "successfully sent #{uniq_phrases.size.to_s.ljust(4)} phrases for namespace '#{namespace}'" if validate_response(response)
+		rescue StandardError => e
+			puts "error: unable to send phrases for namespace '#{namespace}' - #{e.message}"
+		end
 	else
 		puts "error: namespace '#{namespace}' doesn't exist on the localization app"
 	end
