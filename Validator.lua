6317d6aae64e9bb757447400d396f44be87a959a
kollektiv <kollektiv@pc>
2010-03-17 17:27:49 -0400

    Remove redundant string prepend in validator

--- Validator.lua
+++ Validator.lua
@@ -221,17 +221,17 @@ local function err(msg, errlvl, ...)
 		end
 		work[#work+1] = key
 	end
-	error("DXE:ValidateOptions() "..concat(work)..msg, errlvl+2)
+	error("DXE:ValidateOptions() "..concat(work)..": "..msg, errlvl+2)
 end
 
 local function validateIsArray(tbl,errlvl,...)
 	errlvl = (errlvl or 0)+1
 	if #tbl < 1 then
-		err(": table should be an array - got an empty table",errlvl,...)
+		err("table should be an array - got an empty table",errlvl,...)
 	end
 	for k in pairs(tbl) do
 		if type(k) ~= "number" then
-			err(": all keys should be numbers - invalid array",errlvl,...)
+			err("all keys should be numbers - invalid array",errlvl,...)
 		end
 	end
 end
@@ -240,7 +240,7 @@ local function validateVal(v, oktypes, errlvl, ...)
 	errlvl = (errlvl or 0)+1
 	local isok=oktypes[type(v)]
 	if not isok then
-		err(": expected a "..oktypes._..", got '"..tostring(v).."'", errlvl, ...)
+		err("expected a "..oktypes._..", got '"..tostring(v).."'", errlvl, ...)
 	end
 end
 
@@ -250,7 +250,7 @@ local function validateReplaceFuncs(data,text,errlvl,...)
 		local func = match(rep,"&(.+)&")
 		if func:find("|") then func = match(func,"^([^|]+)|(.+)") end
 		if not RepFuncs[func] then
-			err(": replace func does not exist, got '"..rep.."'",errlvl,...)
+			err("replace func does not exist, got '"..rep.."'",errlvl,...)
 		end
 	end
 end
@@ -260,7 +260,7 @@ local function validateReplaceNums(data,text,errlvl,...)
 	for var in gmatch(text,"%b##") do 
 		local key = tonumber(match(var,"#(%d+)#"))
 		if not key or key < 1 or key > 11 then
-			err(": replace num is invalid. it should be [1,11] - got '"..var.."'",errlvl,...)
+			err("replace num is invalid. it should be [1,11] - got '"..var.."'",errlvl,...)
 		end
 	end
 end
@@ -270,9 +270,37 @@ local function validateReplaceVars(data,text,errlvl,...)
 	for var in gmatch(text,"%b<>") do 
 		local key = match(var,"<(.+)>")
 		if not data.userdata[key] then
-			err(": replace var does not exist, got '"..var.."'",errlvl,...)
+			err("replace var does not exist, got '"..var.."'",errlvl,...)
 		end
 	end
+end
+
+local function unclosed_helper(text,errlvl,left,right,...)
+	assert(left and right)
+
+	local work = text
+	-- check for opening
+	if work:find(left) then
+		while work ~= "" do
+			-- balanced match (discard)
+			local rest = work:match("%b"..left..right.."(.*)")
+			if rest then
+				-- check the rest of the string
+				if not rest:find(left) then break end
+				work = rest
+			else
+				err("unclosed "..left..right.." replace, got '"..text.."'",errlvl,...)
+			end
+		end
+	end
+end
+
+local function checkForUnclosedReplaces(data,text,errlvl,...)
+	errlvl=(errlvl or 0)+1
+
+	unclosed_helper(text,errlvl,'#','#',...)
+	unclosed_helper(text,errlvl,'<','>',...)
+	unclosed_helper(text,errlvl,'&','&',...)
 end
 
 local function validateReplaces(data,text,errlvl,...)
@@ -287,7 +315,7 @@ end
 local function validateTracing(tbl,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	if #tbl > 4 or #tbl == 0 then
-		err(": not an array with 1 <= size <= 4",errlvl,"tracing",...)
+		err("not an array with 1 <= size <= 4",errlvl,"tracing",...)
 	end
 	for k,v in ipairs(tbl) do
 		validateVal(v,isnumber,errlvl,"tracing",...)
@@ -304,7 +332,7 @@ local function validateSortedTracing(tbl,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	validateIsArray(tbl,errlvl,"sortedtracing",...)
 	if #tbl == 0 then
-		err(": got an empty array",errlvl,"sortedtracing",...)
+		err("got an empty array",errlvl,"sortedtracing",...)
 	end
 	for k,v in ipairs(tbl) do
 		validateVal(v,isnumber,errlvl,"sortedtracing",...)
@@ -315,7 +343,7 @@ local function validateUnitTracing(tbl,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	validateIsArray(tbl,errlvl,"unittracing",...)
 	if #tbl == 0 then
-		err(": got an empty array",errlvl,"unittracing",...)
+		err("got an empty array",errlvl,"unittracing",...)
 	end
 	for k,v in ipairs(tbl) do
 		validateVal(v,isstring,errlvl,"unittracing",...)
@@ -327,43 +355,43 @@ local validateCommandLine, validateCommandList, validateCommandBundle
 function validateCommandLine(data,type,info,errlvl,...)
 	local oktype = baseLineKeys[type]
 	if not oktype then
-		err(": unknown command line type",errlvl,...)
+		err("unknown command line type",errlvl,...)
 	end
 	validateVal(info,oktype,errlvl,type,...)
 	if type == "expect" then
 		validateIsArray(info,errlvl,type,...)
 		if #info ~= 3 then
-			err(": not an array of size 3",errlvl,type,...)
+			err("not an array of size 3",errlvl,type,...)
 		end
 		for _,str in ipairs(info) do
 			validateVal(str,isstring,errlvl,type,...)
 		end
 		if not conditions[info[2]] then
-			err(": unknown condition",errlvl,type,...)
+			err("unknown condition",errlvl,type,...)
 		end
 		validateReplaces(data,info[1],errlvl,type,...)
 		validateReplaces(data,info[3],errlvl,type,...)
 	elseif type == "set" then
 		for var,value in pairs(info) do
 			if not data.userdata or not data.userdata[var] then
-				err(": setting a non-existent userdata variable '"..var.."'",errlvl,type,...)
+				err("setting a non-existent userdata variable '"..var.."'",errlvl,type,...)
 			end
 			if _G.type(value) == "string" then
 				validateReplaces(data,value,errlvl,var,type,...)
 			elseif _G.type(value) == "table" then
 				if value.type ~= "series" and value.type ~= "container" then
-					err(": invalid userdata table variable expected 'container' or 'series'",errlvl,type,...)
+					err("invalid userdata table variable expected 'container' or 'series'",errlvl,type,...)
 				end
 			end
 		end
 	elseif type == "alert" or type == "quash" then
 		if not data.alerts or not data.alerts[info] then
-			err(": firing/quashing a non-existent alert '"..info.."'",errlvl,type,...)
+			err("firing/quashing a non-existent alert '"..info.."'",errlvl,type,...)
 		end
 	elseif type == "scheduletimer" then
 		validateIsArray(info,errlvl,"scheduletimer",...)
 		if #info ~= 2 then
-			err(": array is not size 2",errlvl,type,...)
+			err("array is not size 2",errlvl,type,...)
 		end
 		local timer,time = info[1],info[2]
 		validateVal(timer,isstring,errlvl,type,...)
@@ -371,43 +399,43 @@ function validateCommandLine(data,type,info,errlvl,...)
 		if _G.type(time) == "string" then
 			validateReplaces(data,time,errlvl,type,...)
 		elseif time < 0 then
-			err(": scheduling a timer < 0 '"..info[1].."'",errlvl,type,...)
+			err("scheduling a timer < 0 '"..info[1].."'",errlvl,type,...)
 		end
 		if not data.timers or not data.timers[timer] then
-			err(": scheduling a non-existent timer '"..info[1].."'",errlvl,type,...)
+			err("scheduling a non-existent timer '"..info[1].."'",errlvl,type,...)
 		end
 	elseif type == "canceltimer" then
 		if not data.timers or not data.timers[info] then
-			err(": canceling a non-existent timer '"..info.."'",errlvl,type,...)
+			err("canceling a non-existent timer '"..info.."'",errlvl,type,...)
 		end
 	elseif type == "tracing" then
 		validateTracing(info,errlvl,...)
 	elseif type == "proximitycheck" or type == "outproximitycheck" then
 		validateIsArray(info,errlvl,type,...)
 		if #info ~= 2 then
-			err(": array is not size 2",errlvl,type,...)
+			err("array is not size 2",errlvl,type,...)
 		end
 		local target,range = info[1],info[2]
 		validateVal(target,isstring,errlvl,type,...)
 		validateVal(range,isnumber,errlvl,type,...)
 		if not target:find("^#[1-7]#$") and not target:find("^&[^&]+&$") then
-			err(": invalid target, has to be exactly of the form #[1-7]# or &func& - got '"..target.."'",errlvl,type,...)
+			err("invalid target, has to be exactly of the form #[1-7]# or &func& - got '"..target.."'",errlvl,type,...)
 		end
 		if target:find("^&[^&]+&$") then
 			validateReplaceFuncs(data,target,errlvl,type,...)
 		end
 		if not ProximityFuncs[range] then
-			err(": invalid range - got '"..range.."'",errlvl,type,...)
+			err("invalid range - got '"..range.."'",errlvl,type,...)
 		end
 	elseif type == "arrow" then
 		if not data.arrows or not data.arrows[info] then
-			err(": starting/removing a non-existent arrow '"..info.."'",errlvl,type,...)
+			err("starting/removing a non-existent arrow '"..info.."'",errlvl,type,...)
 		end
 	elseif type == "removearrow" then
 		validateReplaces(data,info,errlvl,type,...)
 	elseif type == "raidicon" then
 		if not data.raidicons or not data.raidicons[info] then
-			err(": starting a non-existent raid icon '"..info.."'",errlvl,type,...)
+			err("starting a non-existent raid icon '"..info.."'",errlvl,type,...)
 		end
 	elseif type == "removeraidicon" then
 		validateReplaces(data,info,errlvl,type,...)
@@ -431,7 +459,7 @@ function validateCommandBundle(data,bundle,errlvl,...)
 		validateVal(list,istable,errlvl,k,...)
 		validateIsArray(list,errlvl,k,...)
 		if #list < 2 or #list % 2 == 1 then
-			err(": command list size is < 2 or size is odd",errlvl,k,...)
+			err("command list size is < 2 or size is odd",errlvl,k,...)
 		end
 		validateCommandList(data,list,errlvl,k,...)
 	end
@@ -442,7 +470,7 @@ local function validateAlert(data,info,errlvl,...)
 	-- Consistency check
 	for k in pairs(info) do
 		if not alertBaseKeys[k] then
-			err(": unknown key '"..k.."'",errlvl,tostring(k),...)
+			err("unknown key '"..k.."'",errlvl,tostring(k),...)
 		end
 	end
 
@@ -451,25 +479,25 @@ local function validateAlert(data,info,errlvl,...)
 		if info[k] then
 			-- check type
 			if k == "type" and not alertTypeValues[info[k]] then
-				err(": expected simple, dropdown, or centerpopup - got '"..info[k].."'",errlvl,k,...)
+				err("expected simple, dropdown, or centerpopup - got '"..info[k].."'",errlvl,k,...)
 			-- check sounds
 			elseif k == "sound" and not info[k]:find("ALERT%d+") then
-				err(": unknown sound '"..info[k].."'",errlvl,k,...)
+				err("unknown sound '"..info[k].."'",errlvl,k,...)
 			-- check colors
 			elseif (k == "color1" or k == "color2") and not Colors[info[k]] then
-				err(": unknown color '"..info[k].."'",errlvl,k,...)
+				err("unknown color '"..info[k].."'",errlvl,k,...)
 			-- check replaces
 			elseif k == "time" or k == "text" or k == "npcid" or k == "spellid" then
 				validateReplaces(data,info[k],errlvl,k,...)
 			elseif k == "values" then
 				for spellid, total in pairs(info[k]) do
 					if type(spellid) ~= "number" then
-						err(": keys need to be valid spellids - got 'spellid'",errlvl, k, ...)
+						err("keys need to be valid spellids - got 'spellid'",errlvl, k, ...)
 					end
 					validateVal(total,isnumber,errlvl,spellid,k,...)
 					local exists = GetSpellInfo(spellid)
 					if not exists then
-						err(": ["..spellid.."]: unknown spell identifier",errlvl,k,...)
+						err("["..spellid.."]: unknown spell identifier",errlvl,k,...)
 					end
 				end
 			end
@@ -478,11 +506,11 @@ local function validateAlert(data,info,errlvl,...)
 
 	-- color1 is not optional for centerpopups and dropdowns
 	if (info.type == "centerpopup" or info.type == "dropdown") and type(info.color1) ~= "string" then
-		err(": requires color1 to be set",errlvl,...)
+		err("requires color1 to be set",errlvl,...)
 	end
 
 	if info.type == "absorb" and (not info.npcid or not info.values) then
-		err(": absorb bars require npcid and values to be set",errlvl,...)
+		err("absorb bars require npcid and values to be set",errlvl,...)
 	end
 end
 
@@ -498,7 +526,7 @@ local function validateArrow(data,info,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	for k in pairs(info) do
 		if not arrowBaseKeys[k] then
-			err(": unknown key '"..k.."'",errlvl,tostring(k),...)
+			err("unknown key '"..k.."'",errlvl,tostring(k),...)
 		end
 	end
 	for k,oktypes in pairs(arrowBaseKeys) do
@@ -506,11 +534,11 @@ local function validateArrow(data,info,errlvl,...)
 		if info[k] then
 			-- check type
 			if k == "type" and not arrowTypeValues[info[k]] then
-				err(": expected AWAY or TOWARD - got '"..info[k].."'",errlvl,k,...)
+				err("expected AWAY or TOWARD - got '"..info[k].."'",errlvl,k,...)
 			elseif k == "unit" then
 				validateReplaces(data,info[k],errlvl,k,...)
 			elseif k == "sound" and not info[k]:find("ALERT%d+") then
-				err(": unknown sound '"..info[k].."'",errlvl,k,...)
+				err("unknown sound '"..info[k].."'",errlvl,k,...)
 			end
 		end
 	end
@@ -528,7 +556,7 @@ local function validateRaidIcon(data,info,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	for k in pairs(info) do
 		if not raidIconBaseKeys[k] then
-			err(": unknown key '"..k.."'",errlvl,tostring(k),...)
+			err("unknown key '"..k.."'",errlvl,tostring(k),...)
 		end
 	end
 	for k,oktypes in pairs(raidIconBaseKeys) do
@@ -536,18 +564,18 @@ local function validateRaidIcon(data,info,errlvl,...)
 		if info[k] then
 			-- check type
 			if k == "type" and not raidIconTypeValues[info[k]] then
-				err(": expected FRIENDLY or ENEMY - got '"..info[k].."'",errlvl,k,...)
+				err("expected FRIENDLY or ENEMY - got '"..info[k].."'",errlvl,k,...)
 			elseif k == "unit" then
 				validateReplaces(data,info[k],errlvl,k,...)
 			elseif k == "icon" and (info[k] > 8 or info[k] < 1)  then
-				err(": expected icon to be [1-8] - got '"..info[k].."'",errlvl,k,...)
+				err("expected icon to be [1-8] - got '"..info[k].."'",errlvl,k,...)
 			elseif k == "type" then
 				if info[k] == "MULTIFRIENDLY" or info[k] == "MULTIENEMY" then
 					if not info.reset then
-						err(": expected 'reset' to exist", errlvl,k,...)
+						err("expected 'reset' to exist", errlvl,k,...)
 					end
 					if not info.total then
-						err(": expected 'total' to exist", errlvl,k,...)
+						err("expected 'total' to exist", errlvl,k,...)
 					end
 				end
 			end
@@ -567,7 +595,7 @@ local function validateAnnounce(data,info,errlvl,...)
 	errlvl=(errlvl or 0)+1
 	for k in pairs(info) do
 		if not announceBaseKeys[k] then
-			err(": unknown key '"..k.."'",errlvl,tostring(k),...)
+			err("unknown key '"..k.."'",errlvl,tostring(k),...)
 		end
 	end
 	for k,oktypes in pairs(announceBaseKeys) do
@@ -575,7 +603,7 @@ local function validateAnnounce(data,info,errlvl,...)
 		if info[k] then
 			-- check type
 			if k == "type" and not announceTypeValues[info[k]] then
-				err(": expected SAY - got '"..info[k].."'",errlvl,k,...)
+				err("expected SAY - got '"..info[k].."'",errlvl,k,...)
 			end
 		end
 	end
@@ -613,7 +641,7 @@ local function validateUserData(data,info,errlvl,...)
 			validateReplaceNums(data,value,errlvl,...)
 		elseif _G.type(value) == "table" then
 			if value.type ~= "series" and value.type ~= "container" then
-				err(": invalid userdata table variable expected 'container' or 'series'",errlvl,"type",...)
+				err("invalid userdata table variable expected 'container' or 'series'",errlvl,"type",...)
 			end
 		end
 	end
@@ -622,20 +650,20 @@ end
 local function validateEvent(data,info,errlvl,...)
 	for k in pairs(info) do
 		if not eventBaseKeys[k] then
-			err(": unknown parameter",errlvl,k,...)
+			err("unknown parameter",errlvl,k,...)
 		end
 	end
 	for k, oktypes in pairs(eventBaseKeys) do
 		validateVal(info[k],oktypes,errlvl,k,...)
 		if k == "type" then
 			if info.type == "event" and not info.event then
-				err(": missing event key",errlvl,k,...)
+				err("missing event key",errlvl,k,...)
 			end
 			if info.type == "combatevent" and not info.eventtype then
-				err(": missing eventtype key",errlvl,k,...)
+				err("missing eventtype key",errlvl,k,...)
 			end
 			if info.eventtype and not eventtypes[info.eventtype] then
-				err(": invalid eventtype value - got '"..info.eventtype.."'",errlvl,"eventtype",...)
+				err("invalid eventtype value - got '"..info.eventtype.."'",errlvl,"eventtype",...)
 			end
 		elseif k == "spellid" or k == "spellid2" then
 			validateSpellID(data,info,errlvl,k,...)
@@ -659,7 +687,7 @@ local function validate(data,errlvl,...)
 	-- Consistency check
 	for k in pairs(data) do
 		if not baseKeys[k] then
-			err(": unknown key '"..k.."'",errlvl,tostring(k),...)
+			err("unknown key '"..k.."'",errlvl,tostring(k),...)
 		end
 	end
 	-- Base keys
@@ -697,13 +725,13 @@ local function validate(data,errlvl,...)
 					if (onactivate.tracing and onactivate.sortedtracing) 
 						 or (onactivate.tracing and onactivate.unittracing) 
 						 or (onactivate.sortedtracing and onactivate.unittracing) then
-						err(": cannot have tracing and/or sortedtracing and/or unittracing at the same time",errlvl,tblName,...)
+						err("cannot have tracing and/or sortedtracing and/or unittracing at the same time",errlvl,tblName,...)
 					end
 				end
 			end
 			for k in pairs(data[tblName]) do
 				if not tbl[k] then
-					err(": unknown key - got '"..k.."'",errlvl,k,tblName,...)
+					err("unknown key - got '"..k.."'",errlvl,k,tblName,...)
 				end
 			end
 		end
